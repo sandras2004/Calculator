2d3201514cd2ed01ba7ce1a31aae6bb37f016c13
sandra004024 <sandralearning2004@gmail.com>
2024-08-28 18:42:54 +0530

    script added

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 function sum(){
     var fnumber=parseInt(document.getElementById('num1').value)
     var snumber=parseInt(document.getElementById('num2').value)
-    alert(fnumber+snumber)
+    alert('fnumber+snumber')
 }
 function difference(){
     var fnumber=parseInt(document.getElementById('num1').value)
     var snumber=parseInt(document.getElementById('num2').value)
-    alert(fnumber-snumber)
+    alert('fnumber-snumber')
 }
 function multiplication(){
     var fnumber=parseInt(document.getElementById('num1').value)
     var snumber=parseInt(document.getElementById('num2').value)
-    alert(fnumber*snumber)
+    alert('fnumber*snumber')
 }
 function division(){
     var fnumber=parseInt(document.getElementById('num1').value)
     var snumber=parseInt(document.getElementById('num2').value)
-    alert(fnumber/snumber)
+    alert('fnumber/snumber')
 }
