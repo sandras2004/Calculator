32484ff9100a352825d09cb1b208eed20a9dc658
sandra004024 <sandralearning2004@gmail.com>
2024-08-29 18:39:33 +0530

    scripts added

--- js/script.js
+++ js/script.js
@@ -16,5 +16,11 @@ function Multiplication(){
 function Division(){
     var fnumber=parseInt(document.getElementById('num1').value)
     var snumber=parseInt(document.getElementById('num2').value)
-    alert(fnumber/snumber)
+    if(snumber===0){
+        alert('Divisible by zero is not possible')
+    }
+    else{
+        alert(fnumber/snumber)
+    }
+    
 }
